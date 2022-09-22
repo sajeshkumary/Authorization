365721ebeb82a64edb054da8eca098d4b51c6c08
Sajesh Kumar <sajesh_kumar@cms.co.in>
2022-09-22 18:16:41 +0530

    first

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,101 @@
+﻿using Authorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using ServiceBookingLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorizationMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        
+        public AuthorizationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = await AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                
+                response = Ok(new LoginResponse{ token = tokenString, User_Id = user.Usename ,Role = user.Role});
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(User userInfo)
+        {
+
+            if (userInfo is null)
+            {
+                throw new ArgumentNullException(nameof(userInfo));
+            }
+            List<Claim> claims = new List<Claim>();
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            claims.Add(new Claim("Username", userInfo.Usename));
+            claims.Add(new Claim("role", "admin"));
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(2),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<User> AuthenticateUser(Login login)
+        {
+
+            string Baseurl = "https://localhost:7017/";
+            
+            User usrInfo = new User();
+
+            using (var client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(login),
+                    Encoding.UTF8, "application/json");
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                using (var Res = await client.PostAsync("api/User/Login",
+                        content))
+                {
+                    string apiResponse = await Res.Content.ReadAsStringAsync();
+                    usrInfo = JsonConvert.DeserializeObject<User>(apiResponse);
+                }
+            }
+            
+            return usrInfo;
+        }
+    }
+}
